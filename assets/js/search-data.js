0cb031f0f3fd0af6c3488144e0a6c1bece46dfee
HowellLu <95498307+HowellLu@users.noreply.github.com>
2026-01-03 08:50:52 +0000

    Deploying to gh-pages from @ HowellLu/HowellLu.github.io@c7002fbb2be269914c513988423dd98ea6e6e19e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,9 +429,9 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-my-college-voyage",
+          title: 'My College Voyage',
+          description: "A Website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-simply-included",
