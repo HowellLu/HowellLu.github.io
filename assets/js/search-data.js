43e50b89fe9a2fd3cccefde647e8bd0081825c43
HowellLu <95498307+HowellLu@users.noreply.github.com>
2026-01-04 06:36:35 +0000

    Deploying to gh-pages from @ HowellLu/HowellLu.github.io@e6a5d0c1b1052b46a6d668b383b7c81ce0ae225e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,9 +414,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-",
-          title: '',
-          description: "",
+            },},{id: "projects-parallelizing-bayesian-trees-algorithmic-insights-from-gpu-bart",
+          title: 'Parallelizing Bayesian Trees - Algorithmic Insights from GPU BART',
+          description: "Somewhat of WIP",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-my-relationship-future",
