c02ef7f68394855fe250dd52fe5c763c0649c0df
HowellLu <95498307+HowellLu@users.noreply.github.com>
2026-01-04 06:23:20 +0000

    Deploying to gh-pages from @ HowellLu/HowellLu.github.io@8d4c1e12ca6d56126f2877a7db68cc66dd35b8f0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,9 +419,9 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-my-relationship-future",
+          title: 'My Relationship Future',
+          description: "A Website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-is-my-degree-worth-it",
